b812eb24924a1074f0f5c7f3d3bc6f0efd74fbfc
GuilhermeGomesszz <107408334+GuilhermeGomesszz@users.noreply.github.com>
2022-11-29 18:19:56 -0300

    add switch and case

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,11 +1,31 @@
 
 
 for (let i =0; i<document.querySelectorAll('.drum').length; i++) {
-
+l
     document.querySelectorAll('.drum')[i].addEventListener('click', handleClick);
 
 function handleClick() {
-    alert('i got clicked')
+   
+   var buttonInnerHTML = this.innerHTML
+   switch (buttonInnerHTML) {
+    case 'w':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+
+        break;
+    case 'a':
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+   
+    default:
+        break;
+   }
+   
+   
+   
+   
+    
+
 }
 }
 
